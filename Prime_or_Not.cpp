98c171829fafb25e13b0f899f4fad3a36967d804
Rishabh Chatterjee <rishabhchatterjee738@gmail.com>
2023-06-24 16:47:59 +0530

    New Commit

---
+++ Prime_or_Not.cpp
@@ -0,0 +1,31 @@
+//Prime Or Not
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	bool prime = true;
+	cout<<"Enter A Number"<<endl;
+	cin>>a;
+	
+	if(a==0 || a==1)
+	{
+		prime = true;
+	}
+	for(int i=2; i*i<=a; i++)
+	{
+		if(a%i==0)
+		{
+			prime = false;
+			break;
+		}
+	}
+	if(prime)
+	{
+		cout<<a<<" is a Prime Number";
+	}
+	else
+	{
+		cout<<a<<" is not a Prime Number";
+	}
+}
