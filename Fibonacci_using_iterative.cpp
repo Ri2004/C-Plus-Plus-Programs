d8b4402d840b9804260184ef52fdb38e80f62f6e
Rishabh Chatterjee <rishabhchatterjee738@gmail.com>
2023-07-11 15:36:58 +0530

    New Commit

---
+++ Fibonacci_using_iterative.cpp
@@ -0,0 +1,25 @@
+//Fibonacci using iterative
+#include<iostream>
+using namespace std;
+
+int check_fibo_iter(int n)
+{
+	int previous_num = 0;
+	int current_num = 1, pre_previous_num, i;
+	
+	for(i=1; i<n; i++)
+	{
+		pre_previous_num = previous_num;
+		previous_num = current_num;
+		current_num = pre_previous_num + previous_num;
+	}
+	return current_num;
+}
+int main()
+{
+	int num;
+	cout<<"Enter A Number"<<endl;
+	cin>>num;
+	
+	cout<<"Fibonacci Of "<<num<<" is: "<<check_fibo_iter(num);
+}
