a295234d39f104a91909e8fe69e49b82ff92b7f0
Rishabh Chatterjee <rishabhchatterjee738@gmail.com>
2023-07-12 12:46:58 +0530

    Modified

--- Fibonacci_using_iterative.cpp
+++ Fibonacci_using_iterative.cpp
@@ -6,7 +6,9 @@ int check_fibo_iter(int n)
 {
 	int previous_num = 0;
 	int current_num = 1, pre_previous_num, i;
-	
+	if (n==0 || n==1)
+		return n;
+
 	for(i=1; i<n; i++)
 	{
 		pre_previous_num = previous_num;
